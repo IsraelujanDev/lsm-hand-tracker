e6676875e67d3b5c90be33277ccc5fb03d729406
code <code_lioko1@hotmail.com>
2025-05-14 19:29:22 -0600

    Changing the project structure to use packages and modules also metadata.py is the original script that was developed before but now is used inside of the package and we expose the main logic using the wrapper on scripts/generate_metadata.py

---
+++ src/lsm_hand_tracker/__init__.py
@@ -0,0 +1,12 @@
+# src/lsm_hand_tracker/__init__.py
+
+"""
+lsm_hand_tracker
+===============
+
+Package for preprocessing, feature extraction, training
+and gesture inference of Mexican Sign Language.
+"""
+
+__version__ = "0.1.0"
+
